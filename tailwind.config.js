c6d7c3fd79d5e15cac1ea1eaa28ad8ef8ed5fa57
sergio-cutone <cutone.sergio@gmail.com>
2021-06-23 11:20:04 -0400

    Updated Tailwind purge settings;

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: [
+    "../**.php",
+    "../**/**.php",
+    "./**/*.php",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
