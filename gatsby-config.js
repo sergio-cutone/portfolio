85cb6a86aff068fa1fcad26dcade5c0aca3eee0e
sergio-cutone <cutone.sergio@gmail.com>
2021-09-16 14:00:12 -0400

    Update favicon; Added app images;

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
         typeName: "WPGraphQL",
         fieldName: "wpgraphql",
         url: "https://gatsby.sergiocutone.com/graphql",
+        icon: "src/img/favicon.jpg",
       },
     },
   ],
