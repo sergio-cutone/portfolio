f3a743f15e037e1eba00d63f892f2057a6967b5b
sergio-cutone <cutone.sergio@gmail.com>
2021-10-26 21:19:05 -0400

    Update gtag plugin config;

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,32 +7,21 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    `gatsby-plugin-postcss`,
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        typeName: "WPGraphQL",
-        fieldName: "wpgraphql",
-        url: "https://gatsby.sergiocutone.com/graphql",
-        icon: "src/img/favicon.jpg",
-      },
-    },
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "G-92GJRSY0KX", // Google Analytics / GA
-          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
           cookie_expires: 0,
+          send_page_view: true,
         },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
@@ -45,5 +34,16 @@ module.exports = {
         },
       },
     },
+    `gatsby-plugin-postcss`,
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "WPGraphQL",
+        fieldName: "wpgraphql",
+        url: "https://gatsby.sergiocutone.com/graphql",
+        icon: "src/img/favicon.jpg",
+      },
+    },
+    `gatsby-plugin-react-helmet`,
   ],
 }
