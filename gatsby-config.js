4a2cca8f95c4eca408641c9d4c22771162cdfb78
sergio-cutone <cutone.sergio@gmail.com>
2021-10-26 21:33:10 -0400

    Update gtag config;

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,12 +26,8 @@ module.exports = {
         // },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
           head: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          anonymize_ip: true,
         },
       },
     },
