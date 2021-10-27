a0ce7527de2b1cbe698b0cc2384b16861fd091c9
sergio-cutone <cutone.sergio@gmail.com>
2021-10-26 21:24:10 -0400

    Update gtag config;

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,11 +18,12 @@ module.exports = {
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-          send_page_view: true,
-        },
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        //   send_page_view: true
+        // },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
